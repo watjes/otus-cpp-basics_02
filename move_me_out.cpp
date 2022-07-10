d691aaa1ba55dba2da72b51f9cf214d698dfbdac
Wladimir Matjunin <matiounine@yandex.ru>
2022-07-11 01:09:42 +0300

    added solution

---
+++ move_me_out.cpp
@@ -0,0 +1,11 @@
+#include "move_me_out.h"
+
+#include <iostream>
+
+void example_function() {
+	std::cout << "Hello from example function!" << std::endl;
+}
+
+void todo_move_me_out() {
+	std::cout << "This function should be located in the separated source file" << std::endl;
+}
---
+++ move_me_out.h
@@ -0,0 +1,4 @@
+#pragma once
+
+void example_function();
+void todo_move_me_out();
